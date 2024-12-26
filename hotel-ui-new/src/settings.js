6f9f481ecf76dfa1d6ace1a370bd5858a7a727f5
shenzhenwang <12648284+shenzhenwang@user.noreply.gitee.com>
2024-12-26 21:53:01 +0800

    前端初始化

---
+++ hotel-ui-new/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '金城快捷酒店后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
